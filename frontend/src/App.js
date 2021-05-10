d0b978211887ed122e78412f364d4dc099e7b59c
Mohamed Elsawy <201727083@std.sci.cu.edu.eg>
2021-05-10 15:07:11 +0200

    adding header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-
+import { Container} from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
-     <h1>Welcome</h1> 
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome</h1> 
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
