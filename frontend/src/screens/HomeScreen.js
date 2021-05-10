a6f9eca88ae3703579428654475bab119aa1a838
Mohamed Elsawy <201727083@std.sci.cu.edu.eg>
2021-05-10 17:15:32 +0200

    make a small server

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,31 @@
+import React , {useState , useEffect} from 'react'
+import {Row , Col} from 'react-bootstrap'
+import Product from '../components/Product.js'
+import axios from 'axios'
+
+const HomeScreen = () => {
+    const [products,setProuducts] = useState([])
+    
+    useEffect(()=>{
+        const fetchProducts = async () =>{
+            const {data} = await axios.get('/api/products')
+            setProuducts(data)
+        }
+        fetchProducts()   
+    },[])
+
+    return (
+        <>
+            <h1>Latest Products</h1>
+            <Row>
+                {products.map(product=>(
+                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                        <Product product={product} />
+                    </Col>
+                ))}
+            </Row>
+        </>
+    )
+}
+
+export default HomeScreen
